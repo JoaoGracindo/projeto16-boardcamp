27ef20edf34527aa27328ce63cbb0e1198f0be5b
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-02-06 22:37:35 -0300

    Feature: server running on port

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import  express from "express";
+import  express, { json } from "express";
+import  dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT;
 
 const app = express();
+app.use(json());
 
-app.listen(5000);
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}...`));
