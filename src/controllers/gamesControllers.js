d6c9b078cb60b9f019551c99043f9f105fe2c82b
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-02-07 15:32:14 -0300

    Feature: post game controller

--- src/controllers/gamesControllers.js
+++ src/controllers/gamesControllers.js
@@ -10,5 +10,14 @@ export async function getGamesController(req, res){
 
 export async function postGamesController(req, res){
 
+    const {name, stockTotal, pricePerDay, image} = req.body;
 
+    try{
+        await database.query('INSERT INTO games (name, "stockTotal", "pricePerDay", image) VALUES ($1, $2, $3, $4);', [name, stockTotal, pricePerDay, image]);
+        return res.sendStatus(201);
+
+    }catch(err){
+        return res.status(500).send(err.message);
+
+    }
 }
