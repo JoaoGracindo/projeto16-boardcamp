8497be12151ad3bf13e6099c8118f5b8fd6871f7
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-02-09 12:44:43 -0300

    Feature: post rentals route

--- src/middlewares/rentalsMiddlewares.js
+++ src/middlewares/rentalsMiddlewares.js
@@ -3,9 +3,19 @@ import database from '../database/database.js';
 export async function postRentalsMiddleware(req, res, next){
 
     const {customerId, gameId, daysRented} = req.body;
-    const validCustomer = await database.query('SELECT * FROM customers WHERE id=$1;', [customerId]);
-    const validGame = await database.query('SELECT * FROM games WHERE id=$1;', [gameId]);
-    const gamesRented = await database.query('SELECT * FROM rentals WHERE gameId=$1 AND returnDate=null', [gameId])
+    let validCustomer;
+    let validGame;    
+    let gamesRented;
+    
+    try{
+        validCustomer = await database.query('SELECT * FROM customers WHERE id=$1;', [customerId]);
+        validGame = await database.query('SELECT * FROM games WHERE id=$1;', [gameId]);
+        gamesRented = await database.query('SELECT * FROM rentals WHERE gameId=$1 AND returnDate=null', [gameId]);
+
+    }catch(err){
+        return res.status(500).send(err.message);
+
+    }
 
     const isValidCustomer = validCustomer.rows[0];
     const gameExists = validGame.rows[0];
