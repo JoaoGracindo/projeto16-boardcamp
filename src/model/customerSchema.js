821c4952d7e1c7b3eb07423955ba4d257382f280
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-02-10 15:08:06 -0300

    FIX: new user CPF logic

--- src/model/customerSchema.js
+++ src/model/customerSchema.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 const schema = Joi.object({
     name: Joi.string().min(1).required(),
     phone: Joi.string().pattern(/[0-9]{10,11}/),
-    cpf: Joi.string().pattern(/[0-9]{11}/),
+    cpf: Joi.string().pattern(/[0-9]{11}/).min(11).max(11).required(),
     birthday: Joi.date()
 });
 
